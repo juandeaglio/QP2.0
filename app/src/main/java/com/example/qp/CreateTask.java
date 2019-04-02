43a47b2092e638a4cdaf0003faf8ede70c01e274
Ethan Collins <ethan.collins335@myci.csuci.edu>
2019-04-01 20:50:53 -0700

    Added date picker and location for create task. Location still needs to be implemented and tested

--- app/src/main/java/com/example/qp/CreateTask.java
+++ app/src/main/java/com/example/qp/CreateTask.java
@@ -1,17 +1,24 @@
 package com.example.qp;
 
+import android.app.DatePickerDialog;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.DatabaseHelper;
 
+import java.util.Calendar;
 import java.util.UUID;
 
 public class CreateTask extends AppCompatActivity {
@@ -19,11 +26,18 @@ public class CreateTask extends AppCompatActivity {
     //Global variable for the array list of tasks
     MainActivity mainActivity = new MainActivity();
     DatabaseHelper db = new DatabaseHelper(this);
+
+    private TextView dueDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListeener;
     Toast toast = null;
+    private String taskDueDateValue = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        this.dueDate = findViewById(R.id.taskDueDate);
+
 
         Button saveTaskBtn = findViewById(R.id.saveTaskButton);
         this.toast = Toast.makeText(this, "Task Successfully Saved!", Toast.LENGTH_SHORT);
@@ -40,6 +54,38 @@ public class CreateTask extends AppCompatActivity {
 
 
         });
+
+        this.dueDate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Calendar cal  = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(CreateTask.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListeener, year, month, day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+
+            }
+
+
+        });
+
+        mDateSetListeener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Log.d("Date Picker", "onDateSet: date " + (month + 1)+ "/" + dayOfMonth + "/" + year);
+                taskDueDateValue = String.valueOf(month + 1) +  "/" + String.valueOf(dayOfMonth) + "/" + String.valueOf(year);
+                dueDate.setText(taskDueDateValue);
+            }
+        };
+
+
+
 
     }
 
@@ -76,10 +122,10 @@ public class CreateTask extends AppCompatActivity {
         EditText priority = (EditText) findViewById(R.id.priorityNum);
         EditText taskNotes = (EditText) findViewById(R.id.taskNotes);
         //TODO: change dueDate so that the input fields are converted into a Date that can be used by Task class.
-        EditText dueDate = (EditText) findViewById(R.id.taskDueDate);
+        TextView dueDate =  findViewById(R.id.taskDueDate);
         UUID taskID = UUID.randomUUID();
 
-        boolean saveCompleted = db.insertData(taskName.getText().toString(), Integer.parseInt(priority.getText().toString()),dueDate.getText().toString(), taskNotes.getText().toString(), 0, taskID);
+        boolean saveCompleted = db.insertData(taskName.getText().toString(), Integer.parseInt(priority.getText().toString()),this.taskDueDateValue, taskNotes.getText().toString(), 0, taskID);
 
         if(saveCompleted == true){
             populateArrayList("Task_Priority", "asc");
