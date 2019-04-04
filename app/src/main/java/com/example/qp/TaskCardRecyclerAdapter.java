b2aa6ff413baeb1b930c1a3bc3d981cbf29a0b7e
Keghvart <keghvart@aol.com>
2019-04-03 23:44:55 -0700

    Added color coded priority for taskCards

--- app/src/main/java/com/example/qp/TaskCardRecyclerAdapter.java
+++ app/src/main/java/com/example/qp/TaskCardRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.example.qp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,9 +15,15 @@ import java.util.ArrayList;
 
 public class TaskCardRecyclerAdapter extends RecyclerView.Adapter<TaskCardRecyclerAdapter.TaskCardViewHolder> {
 
+
     private ArrayList<Task> taskList;
     private Context context;
-
+    private static final int RED = 1;
+    private static final int ORANGE = 2;
+    private static final int YELLOW = 3;
+    private static final int LIGHT_YELLOW = 4;
+    private static final int GREEN = 5
+            ;
     public TaskCardRecyclerAdapter(ArrayList<Task> globalTaskList, MainActivity context) {
         this.taskList = globalTaskList;
         this.context = context;
@@ -59,6 +66,31 @@ public class TaskCardRecyclerAdapter extends RecyclerView.Adapter<TaskCardRecycl
         Task task = this.taskList.get(i);
         final String taskID = taskList.get(i).getTaskId().toString();
         taskCardViewHolder.taskName.setText(task.getTaskName());
+        if (task.getPriority() == RED)
+        {
+            taskCardViewHolder.priority.setTextColor(Color.parseColor("#d32f2f"));
+        }
+
+        else if (task.getPriority() == ORANGE)
+        {
+            taskCardViewHolder.priority.setTextColor(Color.parseColor("#f57c00"));
+        }
+
+        else if (task.getPriority() == YELLOW)
+        {
+            taskCardViewHolder.priority.setTextColor(Color.parseColor("#fbc02d"));
+        }
+
+        else if (task.getPriority() == LIGHT_YELLOW)
+        {
+            taskCardViewHolder.priority.setTextColor(Color.parseColor("#fff263"));
+        }
+
+        else
+        {
+            taskCardViewHolder.priority.setTextColor(Color.parseColor("#388e3c"));
+        }
+
         taskCardViewHolder.priority.setText(Integer.toString(task.getPriority()));
         taskCardViewHolder.dueDate.setText(task.getDueDate());
         taskCardViewHolder.taskCard.setOnClickListener(new View.OnClickListener() {
