89b23484e77968a65a2126cfdd3149ad6eaed15c
Ethan Collins <ethan.collins335@myci.csuci.edu>
2019-04-10 16:14:32 -0700

    Bug fixes

--- app/src/main/java/com/example/qp/CompletedTasks.java
+++ app/src/main/java/com/example/qp/CompletedTasks.java
@@ -13,7 +13,6 @@ public class CompletedTasks extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/qp/MainActivity.java
+++ app/src/main/java/com/example/qp/MainActivity.java
@@ -77,24 +77,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-        //TODO: populate arrayList w/ database entries - Ant
-/*
-        int cursorSize = 0;
-        if(retrievedData.moveToFirst())
-        {
-            cursorSize = 1;
-            while(retrievedData.moveToNext())
-            {
-                cursorSize++;
-            }
-        }
-        databasesize = cursorSize;
-        */
-        //populate();
-        //createTask = new CreateTask();
-        //createTask.populateArrayList("Task_Priority", "asc");
         populateArrayList(this.db);
-
         TaskCardRecyclerAdapter adapter = new TaskCardRecyclerAdapter(globalTaskList, this);
         RecyclerView taskRecycler = (RecyclerView) findViewById(R.id.task_card_recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
