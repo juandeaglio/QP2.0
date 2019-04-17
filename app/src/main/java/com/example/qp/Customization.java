662a99499efbf32a992e01eb7391c96b4be54e25
Volkov <anthony.volkov384@SIE-1222-07.local>
2019-04-17 16:16:33 -0700

    Working on customization screen.

--- app/src/main/java/com/example/qp/Customization.java
+++ app/src/main/java/com/example/qp/Customization.java
@@ -15,6 +15,8 @@ public class Customization extends AppCompatActivity {
         setContentView(R.layout.activity_customization);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        findViewById(R.id.task_card_recycler);
     }
 
 }
