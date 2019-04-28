edd952c112595979d90d401830c790f116d5a179
juandeaglio <retagaine@gmail.com>
2019-04-28 15:35:47 -0700

    Added color changing to most screens. Textviews still need to changed, Calendar needs changing too.

--- app/src/main/java/com/example/qp/MainActivity.java
+++ app/src/main/java/com/example/qp/MainActivity.java
@@ -12,6 +12,7 @@ import static maes.tech.intentanim.CustomIntent.customType;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Canvas;
@@ -37,9 +38,12 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -65,7 +69,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private TaskCardRecyclerAdapter adapter;
     SwipeController swipeController;
     public static ColorManager colorManager;
-
+    public Toolbar toolbar;
+    public FloatingActionButton fab;
     public String sortSelector = "Task_Priority"; // Default sorting priority
 
     @Override
@@ -82,13 +87,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         SharedPreferences preferences = getSharedPreferences("prefs", MODE_PRIVATE);
 
         boolean firstStart = preferences.getBoolean("firstStart", true);
-
+        //boolean firstStart = true;
         if(firstStart){
-
             showfirstTimeDialog();
         }
 
-        //showfirstTimeDialog();
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -99,43 +103,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        colorManager = new ColorManager(this);
-        FloatingActionButton fab = findViewById(R.id.createTaskBtn);
+        colorManager = new ColorManager(MainActivity.this);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(colorManager.getColorAccent());
+
+        fab = findViewById(R.id.createTaskBtn);
+        fab.setBackgroundTintList(ColorStateList.valueOf(colorManager.getColorAccent()));
         fab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 openCreateTaskActivity(view);
             }
         });
 
+
         populateArrayList(this.db, this.sortSelector);
 
         adapter = new TaskCardRecyclerAdapter(globalTaskList, this);
         setUpRecyclerView();
-
     }
 
     private void  showfirstTimeDialog(){
-      new AlertDialog.Builder(this)
-                .setTitle("Welcome")
-                .setMessage("Welcome to Quick Plan! Would you like a walk though?")
-                .setPositiveButton("Next", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Start by creating a temp task
-                        showCreateTaskDialog();
 
-                    }
-                })
-
-                .setNegativeButton("Skip", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                }).create().show();
-
-
-
+        startActivity(new Intent(this, IntroActivity.class));
 
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
         SharedPreferences.Editor editor =  prefs.edit();
@@ -143,33 +133,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         editor.apply();
         }
 
-    private void showCreateTaskDialog() {
-       AlertDialog.Builder alert =  new AlertDialog.Builder(this)
-                .setTitle("First things first")
-                .setMessage("You can create a new task or todo with this button")
-                .setPositiveButton("Next", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Start by creating a temp task
-                        //showCreateTaskDialog();
-                        dialog.dismiss();
-
-                    }
-                })
-
-                .setNegativeButton("Skip", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-
-       AlertDialog dialogWindow = alert.create();
-       dialogWindow.getWindow().setGravity(Gravity.BOTTOM);
-       dialogWindow.getWindow().setLayout(100,200);
-       dialogWindow.show();
-
-    }
 
 
     @Override
@@ -234,11 +197,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onResume()
     {
         super.onResume();
+        populateArrayList(this.db, this.sortSelector);
+
         adapter.updateData();
 
-        int color = colorManager.getColorAccent();
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //toolbar.setBackgroundColor(color);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(colorManager.getColorAccent());
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(colorManager.getColorAccent());
+
+        fab = findViewById(R.id.createTaskBtn);
+        fab.setBackgroundTintList(ColorStateList.valueOf(colorManager.getColorAccent()));
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                openCreateTaskActivity(view);
+            }
+        });
+
     }
 
     public void createNotification(String aMessage, Context context) {
@@ -320,6 +297,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }while (cursor.moveToNext());
 
         }
+
 
     }
 
